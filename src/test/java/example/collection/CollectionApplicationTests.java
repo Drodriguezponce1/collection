9cc4c4dce4654afc80c2a7e5380ef2fe8908b402
Drodriguezponce1 <Danieljrod98@yahoo.com>
2025-06-26 12:33:30 -0700

    started another collection app

---
+++ src/test/java/example/collection/CollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package example.collection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
